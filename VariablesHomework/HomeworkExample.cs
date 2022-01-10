5001b87cf466ee461209cf73aada8956e271f882
Eric Ray <erichamion@gmail.com>
2022-01-10 16:27:57 -0600

    initial commit

---
+++ VariablesHomework/HomeworkExample.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VariablesExample
+{
+    [TestClass]
+    public class Variables
+    {
+        [TestMethod]
+        public void Declaration()
+        {
+            //Assert.IsTrue(foo == 25); // Make this passS
+        }
+
+    }
+}
